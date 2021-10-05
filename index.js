1b4be5a4ab25a150f8241bcfaf573d2d905a950f
Muhammad Abeer <abeersaqib@gmail.com>
2021-10-05 22:33:22 +0500

    fixes

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  adapter : require('./packages/adapter'),
+  bluetooth : require('./packages/bluetooth'),
+  console : require('./packages/console'),
+  network : require('./packages/network'),
+  printer : require('./packages/printer'),
+  screen : require('./packages/screen'),
+  serialport : require('./packages/serialport'),
+  server : require('./packages/server'),
+  usb : require('./packages/usb')
+}
